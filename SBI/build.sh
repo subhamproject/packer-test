5dc8e3a2a99e7f048a52b40154045dbcd2a5cd56
root <root@ip-172-31-22-144.ap-southeast-1.compute.internal>
2020-06-11 09:50:30 +0000

    updated script file

--- SBI/build.sh
+++ SBI/build.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-set -x
-
 export PATH="$PATH:/usr/local/bin"
 export USERID=$(id -u)
 export GROUPID=$(id -g)
@@ -12,13 +10,9 @@ CONTAINER_NAME="tester-$(echo ${JOB_NAME} | tr '/ ' '._').${BRANCH_NAME}"
 [ -n "$CHANGE_ID" ] && CONTAINER_NAME="${CONTAINER_NAME}-PR${CHANGE_ID}"
 CONTAINER_NAME="${CONTAINER_NAME}-${BUILD_ID}"
 
-echo $REGION
-echo $AMI_NAME
-echo $DESCRIPTION
-
 docker-compose -f builder.yml run \
       --rm -w "$WORKSPACE" \
       --name "$CONTAINER_NAME" \
-      slave build -var region=$REGION \
-      -var ami-name=$AMI_NAME \
+      slave build -var region="$REGION" \
+      -var ami-name="$AMI_NAME" \
       -var description="$DESCRIPTION" slave-ami.json
